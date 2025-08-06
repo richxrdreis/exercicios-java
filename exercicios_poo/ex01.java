1663a336fe55764a4ea4da5457c2bfe011f80e35
Richard Reis <richxrdreis@hotmail.com>
2025-08-06 16:54:10 -0300

    solucao utilizando poo

--- exercicios_poo/ex01.java
+++ exercicios_poo/ex01.java
@@ -3,14 +3,48 @@ package exercicios_poo;
 import java.util.Locale;
 import java.util.Scanner;
 
-// Aqui teremos um exemplo SEM o uso do POO - Somente para vermos a diferença
-// Here we will have an example WITHOUT POO - Just to see the difference
+import entities.Triangle; // aqui importamos nosso obj criado - importing our object
+
 public class ex01 {
     public static void main(String[] args) {
         
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        // Aqui teremos a resolucao utilizando POO
+        // Here we will have a resolution using OOP
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.printf("Enter the measures of triangle X: %n");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.printf("Enter the measures of triangle Y: %n");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double p = (x.a + x.b + x.c) / 2;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+        p = (y.a + y.b + y.c) / 2;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle X area: %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        } else {
+            System.out.println("Larger area: Y");
+        }
+
+        /* Aqui teremos um exemplo SEM o uso do POO - Somente para vermos a diferença
+           Here we will have an example WITHOUT POO - Just to see the difference
+         
         double xA, xB, xC, yA, yB, yC;
 
         System.out.printf("Enter the measures of triangle X: %n");
@@ -36,6 +70,7 @@ public class ex01 {
         } else {
             System.out.println("Larger area: Y");
         }
+            */
 
         sc.close();
     }
