97bad3ea0728d188ea59f0a387fb59d9fded883c
Richard Reis <richxrdreis@hotmail.com>
2025-08-07 17:33:45 -0300

    ex do rectangle

---
+++ exercicios_poo/ex03/ex03.java
@@ -0,0 +1,25 @@
+package exercicios_poo.ex03;
+
+import java.util.Locale;
+import java.util.Scanner;
+import exercicios_poo.ex03.rectangle.Rectangle;
+
+public class ex03 {
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle retangulo = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        retangulo.width = sc.nextDouble();
+        retangulo.height = sc.nextDouble();
+
+        System.out.printf("AREA = %.2f%n", retangulo.area());
+        System.out.printf("PERIMETER = %.2f%n", retangulo.perimeter());
+        System.out.printf("DIAGONAL = %.2f%n", retangulo.diagonal());
+
+        sc.close();
+    }
+}
---
+++ exercicios_poo/ex03/rectangle/Rectangle.java
@@ -0,0 +1,18 @@
+package exercicios_poo.ex03.rectangle;
+
+public class Rectangle {
+    public double width;
+    public double height;
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+    
+    public double diagonal() {
+        return Math.sqrt(width * width+ height * height);
+    }
+}
