eecfdc9b875e2d1fa1e46de8a502b44f38fb7064
Richard Reis <richxrdreis@hotmail.com>
2025-08-19 20:29:06 -0300

    vetores + POO

---
+++ exercicios_arrays/ex04/ex04.java
@@ -0,0 +1,42 @@
+package exercicios_arrays.ex04;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex04 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        double soma, media;
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        n = sc.nextInt();
+
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um numero: ");
+            vect[i] = sc.nextDouble();
+        }
+
+        soma = 0.0;
+        for (int i = 0; i < n; i++) {
+            soma += vect[i];
+        }
+
+        media = soma / n;
+
+        System.out.print("VALORES = ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%.1f  ", vect[i]);
+        }
+
+        System.out.printf("\nSOMA = %.2f\n", soma);
+        System.out.printf("MEDIA = %.2f\n", media);
+
+        sc.close();
+    }
+}
---
+++ exercicios_arrays/ex05/ex05.java
@@ -0,0 +1,81 @@
+package exercicios_arrays.ex05;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex05 {
+
+    public static class Pessoa {
+        private String nome;
+        private int idade;
+        private double altura;
+
+        public Pessoa(String nome, int idade, double altura) {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+
+        public double getAltura() {
+            return altura;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantasa pessoas serao digitadas? ");
+        int pessoas = sc.nextInt();
+        sc.nextLine();
+
+        Pessoa[] vetor = new Pessoa[pessoas];
+
+        double somaAltura = 0.0;
+        int menores16 = 0;
+
+        for (int i = 0; i < pessoas; i++) {
+            System.out.println("Dados da " + (i + 1) + "a pessoa:");
+            System.out.print("Nome: ");
+            String nome = sc.nextLine();
+
+            System.out.print("Idade: ");
+            int idade = sc.nextInt();
+
+            System.out.print("Altura: ");
+            double altura = sc.nextDouble();
+            sc.nextLine();
+
+            vetor[i] = new Pessoa(nome, idade, altura);
+
+            somaAltura += altura;
+            if (idade < 16) {
+                menores16++;
+            }
+        }
+
+        double mediaAlturas = somaAltura / pessoas;
+        double percentualMenores = ((double) menores16 / pessoas) * 100.0;
+
+        System.out.printf("\nAltura media = %.2f\n", mediaAlturas);
+        System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", percentualMenores);
+
+        for (Pessoa p : vetor) {
+            if (p.getIdade() < 16) {
+                System.out.println(p.getNome());
+            }
+        }
+
+
+        sc.close();
+    }
+}
