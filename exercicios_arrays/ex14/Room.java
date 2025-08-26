3903d5002a2a12ad71a530ecac0f940bed6a123f
Richard Reis <richxrdreis@hotmail.com>
2025-08-26 16:56:19 -0300

    rooms arrested

---
+++ exercicios_arrays/ex14/Room.java
@@ -0,0 +1,78 @@
+package exercicios_arrays.ex14;
+
+import java.sql.SQLOutput;
+import java.util.Formattable;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Room {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        Rent[] vect = new Rent[10];
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Rent #" + i + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.next();
+            System.out.print("Room: ");
+            int roomNumber = sc.nextInt();
+
+            vect[roomNumber] = new Rent(name, email);
+        }
+
+        System.out.println();
+        System.out.println("Busy rooms: ");
+        for (int i = 0; i < 10; i++) {
+            if (vect[i] != null) {
+                System.out.println(i + ": " + vect[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
+
+class Rent {
+    private String name;
+    private String email;
+
+    public Rent() {
+
+    }
+
+    public Rent(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return
+                name + ", " + email;
+    }
+}
