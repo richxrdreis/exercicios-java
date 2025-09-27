c8068a54de71aeed142b5c7df2c0cad2e69e5952
Richard Reis <richxrdreis@hotmail.com>
2025-09-27 11:39:06 -0300

    localDate

---
+++ data_hora/localDateToText.java
@@ -0,0 +1,28 @@
+package data_hora;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class localDateToText {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T22:45:23");
+        Instant d06 = Instant.parse("2022-07-20T22:45:23Z");
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+
+        System.out.println("d04 = " + d04.format(fmt1)); // chamando o metodo format p colocar o metodo q eu criei acima
+        System.out.println("d04 = " + fmt1.format(d04));
+        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+        System.out.println("d05 = " + d05.format(fmt1));
+        System.out.println("d05 = " + d05.format(fmt2));
+
+        System.out.println("d06 = " + fmt3.format(d06));
+    }
+}
